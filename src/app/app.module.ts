4822d8e57e00ea978492763d197031beafd42572
AnoirsGit <immvp.com@gmail.com>
2020-07-17 12:19:02 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+
+import { ShoppingService } from './shopping-list/shopping-service.service';
+import {RecipeDataService} from './recipes/recipe-data.service';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+
+import { ShoppingListRoutingModule } from './shopping-list/shopping-list-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    
+    
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    // HttpModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+    ShoppingListModule,
+    SharedModule,
+    AuthModule,
+    ShoppingListRoutingModule
+  
   ],
-  providers: [],
+  providers: [ShoppingService , RecipeDataService , 
+    {
+      provide : HTTP_INTERCEPTORS, 
+      useClass : AuthInterceptorService, 
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,6 @@
+import 
+
+
+export function shoppingListReducer(state, action){
+
+}
